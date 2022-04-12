fcbc61edb0e928c4e5dd1395e77286591ef4be0e
z0xyz <solicitmind@gmail.com>
2022-04-12 07:56:49 +0200

    New argument parser module

---
+++ argument-parser.py
@@ -0,0 +1,15 @@
+from os.path import isfile
+import argparse
+
+parser = argparse.ArgumentParser(
+    description='Crack protected pdf files using plain brute force!',
+    allow_abbrev=True
+)
+
+parser.add_argument('-v', '--version', help='Show project version number')
+parser.add_argument('-c', '--check', help='Check the protection on a pdf file(s)')
+parser.add_argument('-s', '--start', help='The start number for brute force')
+parser.add_argument('-e', '--end', help='The end number for brute force')
+parser.add_argument('-p', '--path', help='The path to the pdf file')
+
+argumentsObject = parser.parse_args()
