b3f951a19fb96fb2b5112b43ec2e0a54133d2517
z0xyz <solicitmind@gmail.com>
2022-04-13 16:04:33 +0200

    Modularizing the functions& appending more flags

--- argument-parser.py
+++ argument-parser.py
@@ -11,5 +11,8 @@ parser.add_argument('-c', '--check', help='Check the protection on a pdf file(s)
 parser.add_argument('-s', '--start', help='The start number for brute force')
 parser.add_argument('-e', '--end', help='The end number for brute force')
 parser.add_argument('-p', '--path', help='The path to the pdf file')
+parser.add_argument('-f' '--verbose', help='Show [f]ailed password trial attempts. Default is being suppressed!')
 
 argumentsObject = parser.parse_args()
+
+# Check if the passed filepath is valid
--- main.py
+++ main.py
@@ -1,33 +1,101 @@
 import pikepdf
+from pyparsing import one_of
 
-userPrompt = input('''
-1-Try against a a file of words
-2-Using brute Force
-\t Which one do you wanna choose : ''')
+def prompt_user():
+    userPrompt = input('''
+    1-Try against a a file of words
+    2-Using brute Force
+    \t Which one do you wanna choose :
+''')
 
+some_number_one = 1
+some_number_one_hundred = 100
+verbosity_flag = True
+locked_pdf_file = 'locked-file.pdf'
+some_password = 99
 
-def pdfFileDecrypt(pdfFilePath,userPassword):
+def check_password(file_path, user_password):
+    try:
+        with pikepdf.open(file_path, password=(str(user_password))):
+            return(True)
+    except pikepdf.PasswordError:
+        return(False)
+
+#print(check_password(locked_pdf_file, some_password))
+#print(check_password(locked_pdf_file, 20000))
+
+# We have two functions { brute_crack_password, dict_crack_password}
+#     -They both used to either crack the pdf's passwrd using sheer brute force or a a predefine dictionary lines
+
+def return_password(file_path, number, verbosity_option):
+    if (check_password(file_path, number)):
+        print(f"The Pdf's password is {number}")
+    else:
+        print(f'Password {number} is wrong')
+
+def brute_crack_password(first_number, end_number, file_path):
+    for number in range(first_number, end_number):
+        pass
+
+# brute_crack_password(some_number_one, some_number_one_hundred, locked_pdf_file)
+
+def dict_crack_password(dictionary, file_path):
+    pass
+#Reading the dictionary's content line by line
+#    - First check that the file is textual one.
+#    - Read the file content line by line.
+
+def pdfFileDecrypt(file_path,user_password):
     try :
-        pdfObejct = pikepdf.Pdf.open(pdfFilePath , password=str(userPassword))
-        print(f"The password {userPassword} worked !")
+        pdfObejct = pikepdf.Pdf.open(file_path , password=str(user_password))
+        print(f"The password {user_password} worked !")
         return(True)
     except pikepdf._qpdf.PasswordError:
-        print(f"The password {userPassword} is wrong !")
+        print(f"The password {user_password} is wrong !")
 
-if userPrompt == "1":
-    wordListPath = input("Type the wordList absolute path : ")
-    fileObject = open(wordListPath , "r")
-    wordListWords = fileObject.readlines()
-    fileObject.close()
-    for word in wordListWords:
-        word = word.replace("\n" , "")
-        if pdfFileDecrypt(protectedFilePath , word) == True:
-            break
-        else :
-            pdfFileDecrypt(protectedFilePath , word)
-elif userPrompt == "2":
-    print("\nAdd a range of numbers to search in !")
-    startPoint = input("Starting from : ")
-    endPoint = input("till : ")
-    for passwordItem in range (int(startPoint) ,( int(endPoint)+1)):
-        pdfFileDecrypt( protectedFilePath , passwordItem)
+def check_value():
+    if userPrompt == "1":
+        wordListPath = input("Type the wordList absolute path : ")
+        fileObject = open(wordListPath , "r")
+        wordListWords = fileObject.readlines()
+        fileObject.close()
+        for word in wordListWords:
+            word = word.replace("\n" , "")
+            if pdfFileDecrypt(protectedFilePath , word) == True:
+                break
+            else:
+                pdfFileDecrypt(protectedFilePath , word)
+    elif userPrompt == "2":
+        print("\nAdd a range of numbers to search in !")
+        startPoint = input("Starting from : ")
+        endPoint = input("till : ")
+        for passwordItem in range (int(startPoint) ,( int(endPoint)+1)):
+            pdfFileDecrypt( protectedFilePath , passwordItem)
+
+
+# Todos
+#     -The ability to view the file metadata
+#     -The ability to check the file protection scheme
+#     -the ability to crack the pdf password using either 
+#         -sheer bruteforce.
+#         -Dictionary based.
+
+# - Handling linux signals
+#     -Dedicate a separate module to exit the bruteforcing looping
+
+
+shopping_list = {
+    'bread': 1,
+    'Milk': 2,
+    'Butter': 3,
+    'Coffee': 4
+}
+
+print(shopping_list.items())
+print(type(shopping_list.items()))
+
+def show_list():
+    for item, quantity in shopping_list.items():
+        print(f'The required item is {quantity}x and its name is {item}')
+
+show_list()
